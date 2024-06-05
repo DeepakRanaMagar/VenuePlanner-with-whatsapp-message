9ee28b1aeec665dfac9044099682c5357115e80d
DeepakRanaMagar <dpk.me.123@gmail.com>
2024-06-05 14:37:24 +0545

    login&auth token

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import CustomerRegisterView, VenueRegisterView
+from .views import CustomerRegisterView, VenueRegisterView, VenueLoginView, CustomerLoginView
 
 urlpatterns = [
     path('register/venue/', VenueRegisterView.as_view()),
-    path('register/customer/', CustomerRegisterView.as_view())
+    path('register/customer/', CustomerRegisterView.as_view()),
 
+
+    path('login/venue/', VenueLoginView.as_view()),
+    path('login/customer/', CustomerLoginView.as_view()),
 ]
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.models import Token
 
 from .models import Customer, Venue
 from .serializers import (CustomerRegistrationSerializer,
@@ -73,4 +76,72 @@ class CustomerRegisterView(APIView):
             return Response(
                 serializer.errors,
                 status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+
+class VenueLoginView(APIView):
+    '''
+        Handles the endpoint for logging in the venue
+    '''
+
+    permission_classes = [AllowAny, ]
+
+    def post(self, request):
+        '''
+            Handles POST request for username and password to retrieve auth token
+        '''
+
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            token, created = Token.objects.get_or_create(user=user)
+            return Response(
+                {
+                    "token": token.key,
+                    "user_id": user.id
+                }, status= status.HTTP_200_OK
+            )
+        else:
+            return Response(
+                {
+                    "error": "Login Failed. Invalid Credentials"
+                }, status=status.HTTP_401_UNAUTHORIZED
+            )
+        
+
+
+class CustomerLoginView(APIView):
+    '''
+        Handles the endpoint for logging in the venue
+    '''
+
+    permission_classes = [AllowAny, ]
+
+    def post(self, request):
+        '''
+            Handles POST request for username and password to retrieve auth token
+        '''
+
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            token, created = Token.objects.get_or_create(user=user)
+            return Response(
+                {
+                    "token": token.key,
+                    "user_id": user.id
+                }, status= status.HTTP_200_OK
+            )
+        else:
+            return Response(
+                {
+                    "error": "Login Failed. Invalid Credentials"
+                }, status=status.HTTP_401_UNAUTHORIZED
             )
