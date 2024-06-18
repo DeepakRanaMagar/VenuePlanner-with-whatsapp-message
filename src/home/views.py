4815b13985f4348b29e26fb865c55344e65d94f3
DeepakRanaMagar <dpk.me.123@gmail.com>
2024-06-18 14:05:13 +0545

    venue list endpoint

--- src/home/views.py
+++ src/home/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from accounts.models import Venue
+
+from .serializers import VenueSerializer
+
+
+class VenueView(APIView):
+
+    def get(self, request):
+        venues = Venue.objects.all()
+        serializer = VenueSerializer(venues, many=True)
+        response = {
+            "Venues": serializer.data
+        }
+        return Response(response)
